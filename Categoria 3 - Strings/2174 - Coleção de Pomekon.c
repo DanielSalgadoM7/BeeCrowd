3f12ad68456188510bf4980b7bc53c4a481b6fce
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2024-03-11 21:45:31 -0300

    Solução do problema 2174 - Coleção de Pomekon.c

--- Categoria 3 - Strings/2174 - Coleção de Pomekon.c
+++ Categoria 3 - Strings/2174 - Coleção de Pomekon.c
@@ -9,3 +9,36 @@ As próximas N linhas consistem de uma string S (1 ≤ |S| ≤ 10³), representa
 
 Saída
 Você deverá imprimir: "Falta(m) X pomekon(s).", onde X representa a quantidade Pomekons não capturados.*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int valorN, count = 0;
+    scanf("%d", &valorN);
+    getchar(); // Limpa o buffer de entrada
+
+    char pomekons[1000][1000]; // Array bidimensional para armazenar os nomes dos Pomekons
+
+    // Leitura dos nomes dos Pomekons
+    for (int i = 0; i < valorN; i++) {
+        scanf(" %[^\n]", pomekons[i]);
+    }
+
+    // Comparação dos nomes dos Pomekons para encontrar os repetidos
+    for (int i = 0; i < valorN; i++) {
+        for (int j = i + 1; j < valorN; j++) {
+            if (strcmp(pomekons[i], pomekons[j]) == 0) {
+                count++; // Incrementa o contador se encontrar um Pomekon repetido
+                break; // Interrompe a busca para evitar contar o mesmo Pomekon repetido várias vezes
+            }
+        }
+    }
+
+    // Calcula quantos Pomekons estão faltando para completar a coleção
+    int pomekons_faltando = 151 - (n - count);
+
+    printf("Falta(m) %d pomekon(s).\n", pomekons_faltando);
+
+    return 0;
+}
